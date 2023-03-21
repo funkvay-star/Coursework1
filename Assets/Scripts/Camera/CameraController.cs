317d81f0c9a7202dbda34c2c857f0971fd7a2ae3
funkvay-star <pasha1928374655@gmail.com>
2023-03-22 00:17:48 +0400

    changed camera smoothness and enemies now follow player to attack

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -12,9 +12,14 @@ public class CameraController : MonoBehaviour
 
 	[SerializeField] private float _maxHeight, _minHeight;
 
+	[SerializeField] private Vector2 cameraWindow = new Vector2(2, 2); // Size of the camera window
+
+	[SerializeField] private float smoothTime = 0.3f; // Smoothness factor for interpolation
+
 	private PlayerMovement _playerMov;
 
 	private float _lastXPosition;
+	private Vector3 velocity = Vector3.zero; // Velocity used for interpolation
 
 	// Start is called before the first frame update
 	void Start()
@@ -22,29 +27,36 @@ public class CameraController : MonoBehaviour
 		_lastXPosition = transform.position.x;
 	}
 
+	// FixedUpdate is called at a fixed interval and is independent of frame rate
+	void FixedUpdate()
+	{
+		FollowTarget();
+	}
+
 	// Update is called once per frame
 	void Update()
 	{
-		FollowTarger();
-
 		ChangeBackground();
 	}
 
-	void FollowTarger()
+	void FollowTarget()
 	{
-		transform.position = new Vector3(_target.position.x, Mathf.Clamp(_target.position.y, _minHeight, _maxHeight), transform.position.z);
+		Vector3 targetPosition = new Vector3(_target.position.x, Mathf.Clamp(_target.position.y, _minHeight, _maxHeight), transform.position.z);
+
+		// Check if the player is outside the camera window
+		if (Mathf.Abs(targetPosition.x - transform.position.x) > cameraWindow.x / 2 || Mathf.Abs(targetPosition.y - transform.position.y) > cameraWindow.y / 2)
+		{
+			// Smoothly interpolate the camera position
+			transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+		}
 	}
 
 	void ChangeBackground()
 	{
-		float amountTOMoveX = transform.position.x - _lastXPosition;
-
-		//_farBackBackground.position += new Vector3(amountTOMoveX, 0, 0);
-		//_middleBackground.position += new Vector3(amountTOMoveX * 0.5f, 0, 0);
+		float amountToMoveX = transform.position.x - _lastXPosition;
 
 		_farBackBackground.position = new Vector3(transform.position.x, transform.position.y, 0);
 		_middleBackground.position = 0.75f * new Vector3(transform.position.x, transform.position.y, 0);
-		//_closeBackground.position = new Vector3(0.75f * transform.position.x, 0.6f * transform.position.y, 0);
 
 		_lastXPosition = transform.position.x;
 	}
--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -4,60 +4,74 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    [SerializeField] private int _moveSpeed;
+	[SerializeField] private int _moveSpeed;
 
-    [SerializeField] private Transform _leftPoint, _rightPoint;
+	[SerializeField] private Transform _leftPoint, _rightPoint;
 
-    [SerializeField] bool _lookOtherWay; // if your sprites looks right, then _lookOtherWay = false, and if sprite looks left, then _lookOtherWay = true
+	[SerializeField] private bool _lookOtherWay;
 
-    //[SerializeField] private int _lookOtherWay; // if your sprites looks right, then _lookOtherWay = -1, and if sprite looks left, then _lookOtherWay = 1
+	[SerializeField] private float playerDetectRange = 5.0f;
+	[SerializeField] private float playerStopChasingRange = 5.0f;
 
 	private Animator _animator;
+	private bool _movingRight;
+	private Rigidbody2D _theRB;
+	public SpriteRenderer _theSR;
 
-    private bool _movingRight;
+	public float _waitTime, _moveTime;
+	private float _waitCount, _moveCount;
 
-    private Rigidbody2D _theRB;
-    public SpriteRenderer _theSR;
+	private Transform playerTransform;
 
-    public float _waitTime, _moveTime;
-    private float _waitCount, _moveCount;
+	private enum EnemyState { Patrol, ChasePlayer }
+	private EnemyState currentState;
 
-    private int _counterTest = 0;
+	void Start()
+	{
+		_theRB = GetComponent<Rigidbody2D>();
+		_animator = GetComponent<Animator>();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        _theRB = GetComponent<Rigidbody2D>();
-        _animator = GetComponent<Animator>();
+		_leftPoint.parent = null;
+		_rightPoint.parent = null;
 
-        _leftPoint.parent = null;
-        _rightPoint.parent = null;
+		_movingRight = true;
+		_moveCount = Random.Range(_moveTime * 0.5f, _moveTime * 1.25f);
 
-        _movingRight = true;
-        _moveCount = Random.Range(_moveTime * 0.5f, _moveTime * 1.25f);
+		transform.localScale = new Vector3(ShouldLookOtherWay(_lookOtherWay), 1, 1);
 
-        //_theSR.flipX = _lookOtherWay;
-        transform.localScale = new Vector3(ShouldLookOtherWay(_lookOtherWay), 1, 1);
+		currentState = EnemyState.Patrol;
+
+		playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
-		//Debug.Log("Update t: " + _counterTest++ + " " + (GetComponent<EnemyAttack>()?.IsAttacking ?? false));
-		if (_moveCount > 0) //&& !(GetComponent<EnemyAttack>()?.IsAttacking ?? false)) // '?.' if returns null (GetComponent<EnemyAttack>() then don't throw exception. if GetComponent<EnemyAttack>()?.IsAttacking is null, then '?? false' will return 'false' 
+	void Update()
+	{
+		if (currentState == EnemyState.Patrol)
+		{
+			Patrol();
+		}
+		else if (currentState == EnemyState.ChasePlayer)
+		{
+			ChasePlayer();
+		}
+
+		DetectPlayer();
+	}
+
+	private void Patrol()
+	{
+		if (_moveCount > 0)
 		{
 			_moveCount -= Time.deltaTime;
 
 			if (transform.position.x > _rightPoint.position.x)
 			{
 				_movingRight = false;
-				//_theSR.flipX = !_lookOtherWay;
 				transform.localScale = new Vector3(ShouldLookOtherWay(!_lookOtherWay), 1, 1);
 			}
 			else if (transform.position.x < _leftPoint.position.x)
 			{
 				_movingRight = true;
-				//_theSR.flipX = _lookOtherWay;
 				transform.localScale = new Vector3(ShouldLookOtherWay(_lookOtherWay), 1, 1);
 			}
 
@@ -70,34 +84,92 @@ public class EnemyController : MonoBehaviour
 				_theRB.velocity = new Vector2(-_moveSpeed, _theRB.velocity.y);
 			}
 
-            if(_moveCount <= 0)
-            {
-                _waitCount = Random.Range(_waitTime * 0.75f, _waitTime * 1.25f);
-            }
+			if (_moveCount <= 0)
+			{
+				_waitCount = Random.Range(_waitTime * 0.75f, _waitTime * 1.25f);
+			}
 
-            _animator.SetBool("isMoving", true);
+			_animator.SetBool("isMoving", true);
 		}
-        else if(_waitCount > 0)// && !(GetComponent<EnemyAttack>()?.IsAttacking ?? false))
-        {
-            _waitCount -= Time.deltaTime;
-            _theRB.velocity = new Vector2(0f, _theRB.velocity.y);
+		else if (_waitCount > 0)
+		{
+			_waitCount -= Time.deltaTime;
+			_theRB.velocity = new Vector2(0f, _theRB.velocity.y);
 
-            if (_waitCount <= 0)
-            {
-                _moveCount = Random.Range(_moveTime * 0.5f, _moveTime * 1.25f);
-            }
+			if (_waitCount <= 0)
+			{
+				_moveCount = Random.Range(_moveTime * 0.5f, _moveTime * 1.25f);
+			}
 
 			_animator.SetBool("isMoving", false);
 		}
-        
-  //      if((GetComponent<EnemyAttack>()?.IsAttacking ?? false))
-  //      {
-		//	_theRB.velocity = new Vector2(0f, _theRB.velocity.y);
-		//	_animator.SetBool("isMoving", false);
-		//}
-    }
+	}
 
-    private int ShouldLookOtherWay(bool checker)
+	private void ChasePlayer()
+	{
+		Vector3 direction = (playerTransform.position - transform.position).normalized;
+		float newXPosition = transform.position.x + (direction.x * _moveSpeed * Time.deltaTime);
+
+		if (newXPosition >= _leftPoint.position.x && newXPosition <= _rightPoint.position.x)
+		{
+			_theRB.velocity = new Vector2(direction.x * _moveSpeed, _theRB.velocity.y);
+
+			if (direction.x > 0)
+			{
+				transform.localScale = new Vector3(ShouldLookOtherWay(_lookOtherWay), 1, 1);
+			}
+			else
+			{
+				transform.localScale = new Vector3(ShouldLookOtherWay(!_lookOtherWay), 1, 1);
+			}
+
+			_animator.SetBool("isMoving", true);
+		}
+		else
+		{
+			float horizontalDistance = playerTransform.position.x - transform.position.x;
+			if ((horizontalDistance > 0 && newXPosition < _leftPoint.position.x) || (horizontalDistance < 0 && newXPosition > _rightPoint.position.x))
+			{
+				newXPosition = Mathf.Clamp(newXPosition, _leftPoint.position.x, _rightPoint.position.x);
+				_theRB.velocity = new Vector2(Mathf.Sign(horizontalDistance) * _moveSpeed, _theRB.velocity.y);
+
+				if (Mathf.Sign(horizontalDistance) > 0)
+				{
+					transform.localScale = new Vector3(ShouldLookOtherWay(_lookOtherWay), 1, 1);
+				}
+				else
+				{
+					transform.localScale = new Vector3(ShouldLookOtherWay(!_lookOtherWay), 1, 1);
+				}
+
+				_animator.SetBool("isMoving", true);
+			}
+			else
+			{
+				_theRB.velocity = new Vector2(0f, _theRB.velocity.y);
+				_animator.SetBool("isMoving", false);
+			}
+		}
+	}
+
+
+	private void DetectPlayer()
+	{
+		float distanceToPlayer = Vector3.Distance(transform.position, playerTransform.position);
+
+		if (distanceToPlayer <= playerDetectRange)
+		{
+			currentState = EnemyState.ChasePlayer;
+		}
+		else if (distanceToPlayer > playerStopChasingRange && currentState == EnemyState.ChasePlayer)
+		{
+			currentState = EnemyState.Patrol;
+		}
+	}
+
+
+
+	private int ShouldLookOtherWay(bool checker)
     {
         return checker == true ? -1 : 1;
 	}
